95a5974fe582af82f52e2b3ee91c521d6d26b122
Kaitlin Moreno <kaitmore@gmail.com>
2017-07-14 08:16:10 -0400

    moving reducers to seperate files

--- app/reducers/studentsreducer.jsx
+++ app/reducers/studentsreducer.jsx
@@ -6,6 +6,7 @@ const CREATE = 'CREATE_STUDENT_SUCCESS';
 const FETCH = 'FETCH_STUDENTS_SUCCESS';
 const FETCH_BY_ID = 'FETCH_STUDENT_BY_ID_SUCCESS';
 const WRITE = 'WRITE_STUDENT';
+const EDITING = 'EDITING';
 // const DELETE = 'DELETE_STUDENT_SUCCESS';
 
 /* ------------   ACTION CREATORS     ------------------ */
@@ -13,18 +14,77 @@ const createStudentSuccess = student => ({ type: CREATE, student });
 const fetchStudentsSuccess = students => ({ type: FETCH, students });
 const fetchStudentByIdSuccess = student => ({ type: FETCH_BY_ID, student });
 const writeStudent = ({ name, email, campusId }) => ({ type: WRITE, name, email, campusId });
+const editing = () => ({ type: EDITING })
 
 /* ------------       REDUCER     ------------------ */
-
 const initialState = {
-    students: [],
-    student: { campus: {} },
-    newStudent: {},
-    editing: false
+  students: [],
+  student: { campus: {} },
+  newStudent: {},
+  editing: false
 }
 
-export default function reducer (state = initialState, action) {
- switch (action.type) {
-case: 
- }
+export default function reducer(state = initialState, action) {
+  const newState = Object.assign({}, state)
+  switch (action.type) {
+    case CREATE:
+      newState.students = newState.students.concat(action.student)
+      return newState;
+    case FETCH:
+      newState.students = action.students;
+      return newState;
+    case FETCH_BY_ID:
+      newState.student = action.student;
+      return newState;
+    case WRITE:
+      newState.newStudent.name = action.name;
+      newState.newStudent.email = action.email;
+      newState.newStudent.campusId = action.campusId;
+      return newState; // refactor use a spread operator
+    case EDITING:
+      newState.editing = !state.editing;
+      return newStaute
+  }
 }
+
+/* ------------   THUNK CREATORS     ------------------ */
+export const fetchStudents = () => dispatch => {
+  return axios.get('/api/students')
+    .then(response => {
+      dispatch(fetchStudentsSuccess(response.data))
+    })
+    .catch(error => {
+      throw (error);
+    });
+};
+
+export const fetchStudentById = studentid => dispatch => {
+  return axios.get(`/api/students/${studentid}`)
+    .then(response => {
+      dispatch(fetchStudentByIdSuccess(response.data))
+    })
+    .catch(error => {
+      throw (error);
+    });
+};
+
+export const createStudent = (student, history) => dispatch => {
+  return axios.post('/api/students', student)
+    .then(response => {
+      dispatch(createStudentSuccess(response.data))
+      history.push(`/students/${response.data.id}`)
+    })
+    .catch(error => {
+      throw (error);
+    });
+};
+
+export const updateStudent = (id, student) => dispatch => {
+  return axios.put(`/api/students/${id}`, student)
+    .then(response => {
+      dispatch(updateStudentSuccess(response.data))
+    })
+    .catch(error => {
+      throw (error);
+    });
+};
